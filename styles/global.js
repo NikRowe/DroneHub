b8585b20ca0dc9a1871015d4e0ead05a0dfae95a
Nik Rowe <NikRowe7@Gmail.com>
2020-04-27 22:17:13 -0700

    globalFonts & react-navigagtion install

---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 20,
+        flex: 1,
+    },
+    titleText: {
+        fontFamily: 'bangers-regular',
+        fontSize: 25,
+        color: '#333'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20,
+    }
+})
