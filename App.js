4051e72e8fd6247cd46bf2a81e91b740e9542345
Nik Rowe <NikRowe7@Gmail.com>
2020-04-27 22:02:49 -0700

    Custom Fonts

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import { AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'bangers-regular': require('./assets/fonts/Bangers-Regular.ttf'),
+  'indieFlower-regular': require('./assets/fonts/IndieFlower-Regular.ttf')
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+}
---
+++ screens/home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+export default function Home () {
+    return (
+        <View style={styles.container}>
+            <Text
+                style={styles.titleText}
+            >
+                Home Screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    },
+    titleText: {
+        fontFamily: 'bangers-regular',
+        fontSize: 25,
+    }
+})
